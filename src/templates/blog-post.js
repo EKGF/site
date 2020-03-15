99b8946f752f2ecb90b4ba6effa76d61e48707b3
Jacobus Geluk <jacobus.geluk@agnos.ai>
2020-03-15 13:41:23 +0000

    Added comments to blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,3 +1,11 @@
+/**
+  This file comes from the Gatsby tutorial, it wraps around every Markdown file that it finds under src/.
+  We should extend this into looking like a normal page. Probably think about multiple types of pages:
+  - blog posts / articles
+  - references to external articles plus review comments
+  - references to external ontology sites, dataset providers and so forth (might not be Markdown based but RDF based)
+  - ...
+*/
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
